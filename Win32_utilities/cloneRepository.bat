c366c9ea624f051802c6c899cce7766ebd10ba27
Peter Kraus <peter.kraus54@outlook.de>
2017-08-10 17:13:23 +0200

    batch files for build and test improved

---
+++ Win32_utilities/cloneRepository.bat
@@ -0,0 +1,20 @@
+@echo off
+set DATETIME=%date:~6,4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+set TESTPATH="EvolutionTest_%DATETIME%"
+echo git clone //SYNOLOGYNAS/Peter/SW-projects/Evolution %TESTPATH%
+git clone //SYNOLOGYNAS/Peter/SW-projects/Evolution %TESTPATH%
+if ERRORLEVEL 1 (
+	echo +++ error in git clone
+	goto END
+)
+
+echo cd %TESTPATH%
+cd %TESTPATH%
+if ERRORLEVEL 1 (
+	echo +++ error in cd %TESTPATH%
+	goto END
+)
+
+echo *** ok ***
+:END
+pause
