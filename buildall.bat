6bc77b043ad26a0fdbb621f8bca6dfa630fc2361
Peter Kraus <peter.kraus54@outlook.de>
2017-08-11 00:17:01 +0200

    buildall recognizes PROCESSOR_ARCHITECTURE

--- buildall.bat
+++ buildall.bat
@@ -1,5 +1,9 @@
 @echo off
-echo *** Preparing build environment
+echo *** Preparing build environment for  %PROCESSOR_ARCHITECTURE%
+if %PROCESSOR_ARCHITECTURE% == x86 (
+set ARCHITECTURE=|x86 
+) 
+
 set VCVARS=\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat
 if exist "C:\Program Files%VCVARS%"       call "C:\Program Files%VCVARS%"
 if exist "C:\Program Files (x86)%VCVARS%" call "C:\Program Files (x86)%VCVARS%"
@@ -9,7 +13,7 @@ if ERRORLEVEL 1 (
 )
 
 echo *** Building DEBUG version
-devenv Evolution.sln /build Debug 
+devenv Evolution.sln /build Debug%ARCHITECTURE%
 if ERRORLEVEL 1 (
 	echo +++ error building DEBUG
 	goto ERROR_EXIT
@@ -18,7 +22,7 @@ if ERRORLEVEL 1 (
 echo *** Building RELEASE version
 devenv Evolution.sln /build Release 
 if ERRORLEVEL 1 (
-	echo +++ error building RELEASE
+	echo +++ error building RELEASE%ARCHITECTURE%
 	goto ERROR_EXIT
 )
 
