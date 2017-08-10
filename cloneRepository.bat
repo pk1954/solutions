d1978f4f952cd024cd2ee3f743cfb461e5265ee0
Peter Kraus <peter.kraus54@outlook.de>
2017-08-10 13:14:11 +0200

    improved echo display

--- cloneRepository.bat
+++ cloneRepository.bat
@@ -1,6 +1,8 @@
+rem @echo off
 set DATETIME=%date:~6,4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%
 set TESTPATH="EvolutionTest_%DATETIME%"
-git clone //SYNOLOGYNAS/Peter/Evolution %TESTPATH%
+echo clone //SYNOLOGYNAS/Peter/SW-projects/Evolution %TESTPATH%
+git clone //SYNOLOGYNAS/Peter/SW-projects/Evolution %TESTPATH%
 if ERRORLEVEL 1 (
 	echo +++ error in git clone
 	goto END
