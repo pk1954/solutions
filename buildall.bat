a1221cd5194250d36bce6501f8e3f16e91b3d5ae
Peter Kraus <peter.kraus54@outlook.de>
2017-08-10 13:06:09 +0200

    cloneRepository.bat separated from buildall.bat

--- buildall.bat
+++ buildall.bat
@@ -1,23 +1,9 @@
-set DATETIME=%date:~6,4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%
-set TESTPATH="EvolutionTest_%DATETIME%"
-git clone //SYNOLOGYNAS/Peter/Evolution.git %TESTPATH%
-if ERRORLEVEL 1 (
-	echo +++ error in git clone
-	goto END
-)
-
 set VCVARS=\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat
 if exist "C:\Program Files%VCVARS%"       call "C:\Program Files%VCVARS%"
 if exist "C:\Program Files (x86)%VCVARS%" call "C:\Program Files (x86)%VCVARS%"
 if ERRORLEVEL 1 (
 	echo +++ error in calling vcvars32
 	goto end
-)
-
-cd %TESTPATH%
-if ERRORLEVEL 1 (
-	echo +++ error in cd %TESTPATH%
-	goto END
 )
 
 devenv Evolution.sln /build Debug 
---
+++ cloneRepository.bat
@@ -0,0 +1,17 @@
+set DATETIME=%date:~6,4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+set TESTPATH="EvolutionTest_%DATETIME%"
+git clone //SYNOLOGYNAS/Peter/Evolution.git %TESTPATH%
+if ERRORLEVEL 1 (
+	echo +++ error in git clone
+	goto END
+)
+
+cd %TESTPATH%
+if ERRORLEVEL 1 (
+	echo +++ error in cd %TESTPATH%
+	goto END
+)
+
+echo *** ok ***
+:END
+pause
