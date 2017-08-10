27380297aaf192b3f4e6b446f6c55545e001feff
Peter Kraus <peter.kraus54@outlook.de>
2017-08-10 17:27:37 +0200

    buildall.bat improved

--- buildall.bat
+++ buildall.bat
@@ -1,23 +1,35 @@
+@echo off
+echo *** Preparing build environment
 set VCVARS=\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat
 if exist "C:\Program Files%VCVARS%"       call "C:\Program Files%VCVARS%"
 if exist "C:\Program Files (x86)%VCVARS%" call "C:\Program Files (x86)%VCVARS%"
 if ERRORLEVEL 1 (
 	echo +++ error in calling vcvars32
-	goto end
+	goto ERROR_EXIT
 )
 
+echo *** Building DEBUG version
 devenv Evolution.sln /build Debug 
 if ERRORLEVEL 1 (
-	echo +++ error building Debug
-	goto END
+	echo +++ error building DEBUG
+	goto ERROR_EXIT
 )
 
+echo *** Building RELEASE version
 devenv Evolution.sln /build Release 
 if ERRORLEVEL 1 (
-	echo +++ error building Release
-	goto END
+	echo +++ error building RELEASE
+	goto ERROR_EXIT
 )
 
-echo *** ok ***
-:END
+echo *** Perform COMPARE tests
+cd Compare\TEST
+call TEST_ALL
+if ERRORLEVEL 1 (
+	echo +++ error in COMPARE tests
+	goto ERROR_EXIT
+)
+
+echo *** BUILDALL ok ***
+:ERROR_EXIT
 pause
