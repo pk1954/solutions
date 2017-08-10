03c010143bce08c0a3ab01b563acd8de2b45483e
Peter Kraus <peter.kraus54@outlook.de>
2017-08-10 13:08:09 +0200

    git clone fixed

--- cloneRepository.bat
+++ cloneRepository.bat
@@ -1,6 +1,6 @@
 set DATETIME=%date:~6,4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%
 set TESTPATH="EvolutionTest_%DATETIME%"
-git clone //SYNOLOGYNAS/Peter/Evolution.git %TESTPATH%
+git clone //SYNOLOGYNAS/Peter/Evolution %TESTPATH%
 if ERRORLEVEL 1 (
 	echo +++ error in git clone
 	goto END
