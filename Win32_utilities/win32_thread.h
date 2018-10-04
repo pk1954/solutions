072f7048a0acced23d51e0bc82404dd50b86c89c
Peter Kraus <peter.kraus54@outlook.de>
2018-10-05 00:35:14 +0200

    refactoring

--- Win32_utilities/win32_thread.h
+++ Win32_utilities/win32_thread.h
@@ -3,9 +3,47 @@
 
 #pragma once
 
-#include "windows.h"
+#include "assert.h"
+#include <Windows.h>
+#include <process.h>
+#include "win32_event.h"
 
 namespace Util
 {
-	HANDLE MakeThread( LPTHREAD_START_ROUTINE, LPVOID, LPDWORD );
+	class Thread
+	{
+	public:
+		Thread
+		(
+			unsigned int ( __stdcall *start_address )( void * ),
+			void * parameter
+		) 
+		{
+			m_handle = (HANDLE)_beginthreadex( 0, 0, start_address, parameter, 0, & m_threadId );
+		    assert( m_handle != nullptr );
+		}
+
+		void PostMessage( UINT uiMsg, WPARAM wParam, LPARAM lParam )
+		{
+		    BOOL const bRes = PostThreadMessage( m_threadId, uiMsg, wParam, lParam );
+			DWORD err = GetLastError( );
+			assert( bRes );
+		}
+
+		void SetThreadAffinityMask( DWORD_PTR mask )
+		{
+		    (void)::SetThreadAffinityMask( m_handle, mask );
+		}
+
+		void Wait4Termination( )
+		{
+			WaitForSingleObject( m_handle, INFINITE );   // wait until thread has stopped
+			CloseHandle( m_handle );
+		}
+
+	private:
+		Event  m_eventThreadStarter;
+		HANDLE m_handle;
+		UINT   m_threadId;
+	};
 };
