d23259884fa8f7828f4085005630023322dc4c17
Peter Kraus <peter.kraus54@outlook.de>
2019-09-15 23:18:06 +0200

    added missing files

---
+++ Evolution/GenerationDisplay.cpp
@@ -0,0 +1,8 @@
+// GenerationDisplay.cpp
+//
+// Evolution
+
+#include "stdafx.h"
+#include "GenerationDisplay.h"
+
+wstring const GenerationDisplay::wstringText = L"EvoGen ";
---
+++ Evolution/GenerationDisplay.h
@@ -0,0 +1,54 @@
+// GenerationDisplay.h
+//
+// Evolution
+
+#pragma once
+
+#include "ObserverInterface.h"
+#include "win32_status.h"
+#include "EvoReadBuffer.h"
+#include "EvolutionCore.h"
+
+using std::to_wstring;
+
+class GenerationDisplay : public ObserverInterface
+{
+public:
+	GenerationDisplay
+	(
+		StatusBar     * pStatusBar,
+		EvoReadBuffer * pReadBuffer,
+		int             iPartInStatusBar
+	)
+		:	m_pStatusBar      (pStatusBar),
+			m_pReadBuffer     (pReadBuffer),
+			m_iPartInStatusBar(iPartInStatusBar)
+	{
+		static PIXEL const PIX_WIDTH = PIXEL( ( static_cast<int>(wstringText.size()) + 7) * 9 );   //TODO: magic numbers
+		m_pReadBuffer->RegisterObserver( this );   // notify me, if model has changed
+		m_pStatusBar->CreateRectToolTip
+		( 
+			0, 
+			& PixelRect( 0_PIXEL, 0_PIXEL, PIX_WIDTH, m_pStatusBar->GetHeight( ) ), 
+			L"Generation number. Is incremented at every computed generation, not influenced by editor operations." 
+		);
+		m_pStatusBar->AddCustomControl( PIX_WIDTH ); 
+		Notify( false );
+	}
+
+	virtual void Notify( bool const bImmediately )
+	{
+		EvolutionCore  const * pCore  = m_pReadBuffer->LockReadBuffer( );
+		EVO_GENERATION         evoGen = pCore->GetEvoGenerationNr( );
+		m_pReadBuffer->ReleaseReadBuffer( );
+		m_pStatusBar->DisplayInPart( m_iPartInStatusBar, wstringText + to_wstring( evoGen.GetValue() ) );
+	}
+
+private:
+	static wstring const wstringText;
+
+	StatusBar     * m_pStatusBar;
+	EvoReadBuffer * m_pReadBuffer;
+	int             m_iPartInStatusBar;
+};
+
