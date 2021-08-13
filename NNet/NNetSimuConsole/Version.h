325bca40a2282a3e9360ac3991d779664f27d8f9
Peter Kraus <peter.kraus54@outlook.de>
2021-08-13 13:21:04 +0200

    version 2.0 alpha

--- NNet/NNetSimuConsole/Version.h
+++ NNet/NNetSimuConsole/Version.h
@@ -4,7 +4,7 @@
 
 #define STRINGIZE(s) #s
 
-#define VERSION_MAJOR               1
+#define VERSION_MAJOR               2
 #define VERSION_MINOR               0
 #define VERSION_REVISION            0
 #define VERSION_BUILD               0
