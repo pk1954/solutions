600ca2bcabb744b9d3a91838a2e3533ce17891a2
Peter Kraus <peter.kraus54@outlook.de>
2021-09-12 20:55:39 +0200

    strict rectangle inclusion criterion for pipes

--- NNet/Commands/SelectNobsInRectCommand.h
+++ NNet/Commands/SelectNobsInRectCommand.h
@@ -18,7 +18,22 @@ public:
 	virtual void Do()
 	{ 
 		SelectionCommand::Do();
-		m_pNMWI->GetUPNobs().Apply2AllInRect<Nob>(m_rect, [&](Nob& s){ s.Select(true); });
+		m_pNMWI->GetUPNobs().Apply2AllInRect<Nob>
+		(
+			m_rect, 
+			[&](Nob& s)
+			{ 
+				if (s.IsPipe())
+				{
+					Pipe & pipe { static_cast<Pipe &>(s) };
+					if (!m_rect.Includes(pipe.GetStartPoint()))
+						return;
+					if (!m_rect.Includes(pipe.GetEndPoint()))
+						return;
+				}
+				s.Select(true);
+			}
+		);
 	}
 
 private:
