8630cc4761b6b893c21da9cdf293936c4624f746
Peter Kraus <peter.kraus54@outlook.de>
2021-09-24 22:49:19 +0200

    Added missing files

---
+++ NNet/NNetWindows/NNetWrappers.h
@@ -0,0 +1,10 @@
+// NNetWrappers.h : wrapper functions for unit tests and scripting of NNet
+//
+// NNetWindows
+
+#pragma once
+
+class NNetModelCommands;
+class MainWindow;
+
+extern void DefineNNetWrappers(NNetModelCommands * const);
