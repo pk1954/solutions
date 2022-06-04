3932e9a3b80d36bd4c976299da93831bf2ee8eb5
Peter Kraus <peter.kraus54@outlook.de>
2022-06-04 13:51:35 +0200

    added missing files

---
+++ NNet/Commands/AddSigGen2MonitorCmd.h
@@ -0,0 +1,38 @@
+// AddSigGen2MonitorCmd.h
+//
+// Commands
+
+#pragma once
+
+#include "NNetModelWriterInterface.h"
+#include "SignalGenerator.h"
+#include "SignalFactory.h"
+#include "MonitorData.h"
+#include "NNetCommand.h"
+
+class AddSigGen2MonitorCmd : public NNetCommand
+{
+public:
+	explicit AddSigGen2MonitorCmd(TrackNr const trackNr)
+      : m_trackNr(trackNr)
+	{
+		m_pSigGen = m_pNMWI->GetSigGenSelected();
+	}
+
+	void Do() final 
+	{ 
+		m_pNMWI->GetMonitorData().InsertTrack(m_trackNr);
+		m_signalId = SignalFactory::MakeSigGenSignal(m_trackNr, *m_pNMWI);
+	}
+
+	void Undo() final
+	{ 
+		m_pNMWI->GetMonitorData().DeleteSignal(m_signalId);
+		m_pNMWI->GetMonitorData().DeleteTrack(m_trackNr);
+	};
+
+private:
+	SignalGenerator const * m_pSigGen;
+	TrackNr          const m_trackNr;
+	SignalId               m_signalId {};
+};
