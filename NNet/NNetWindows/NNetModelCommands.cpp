4ebb74ff8e4ae2de6b7b330e3367fc332da0507b
Peter Kraus <peter.kraus54@outlook.de>
2022-08-19 16:03:14 +0200

    Connect2BaseKnotCommand.ixx

--- NNet/NNetWindows/NNetModelCommands.cpp
+++ NNet/NNetWindows/NNetModelCommands.cpp
@@ -11,7 +11,6 @@ module;
 #include "CommandStack.h"
 #include "CommandFunctions.h"
 #include "ConnAnimationCommand.h"
-#include "Connect2BaseKnotCommand.h"
 #include "Connect2PipeCommand.h"
 #include "CopySelectedNobs.h"
 #include "InputConnector.h"
@@ -70,6 +69,7 @@ import SetTargetNobCmd;
 import NNetModelCommands;
 import SetHighlightedNobCmd;
 import AttachSigGen2ConnCmd;
+import Connect2BaseKnotCommand;
 
 using std::wcout;
 using std::endl;
